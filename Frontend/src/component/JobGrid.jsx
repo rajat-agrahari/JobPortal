b5e17d49d5f0391322c7a36611690e9150d38609
Rajat Gupta <rajatkiet96@gmail.com>
2024-09-04 21:38:04 +0530

    add Jobs grid card component

---
+++ Frontend/src/component/JobGrid.jsx
@@ -0,0 +1,125 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+const JobGrid = () => {
+  return (
+    <div className="row justify-content-center m-5">
+      <div className="col-md-5 col-lg-4 mb-3 mb-sm-1 m-4">
+        <div className="card shadow">
+          <div className="card-body">
+            <span className="badge text-bg-secondary mb-2">New</span>
+
+            <h5 className="card-title mb-0">Php Web devloper</h5>
+            <span>Umberala Infocare</span>
+            <div className="row mt-2 mb-1">
+              <div className="col-3 ">
+                <span className="badge text-bg-info">
+                  <i className="bi bi-briefcase me-1"></i>
+                  0-2 Yrs
+                </span>
+              </div>
+
+              <div className="col-3 ">
+                <span className="badge text-bg-success">
+                  <i className="bi bi-currency-rupee"></i>
+                  35000
+                </span>
+              </div>
+              <div className="col-3 ">
+                <span className="badge text-bg-dark">
+                  <i className="bi bi-geo-alt me-1"></i>Noida
+                </span>
+              </div>
+            </div>
+            <p className="card-text lh-sm mb-2">
+              He/she should have a creative & thoughtful approach to requests &
+              collaborate...
+            </p>
+            <small className="d-block">2 days ago</small>
+            <a href="#" className="btn btn-primary mt-2">
+              Apply
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-5 col-lg-4 mb-3 mb-sm-1 m-4">
+        <div className="card shadow">
+          <div className="card-body">
+            <span className="badge text-bg-secondary mb-2">New</span>
+
+            <h5 className="card-title mb-0">Php Web devloper</h5>
+            <span>Umberala Infocare</span>
+            <div className="row mt-2 mb-1">
+              <div className="col-3 ">
+                <span className="badge text-bg-info">
+                  <i className="bi bi-briefcase me-1"></i>
+                  0-2 Yrs
+                </span>
+              </div>
+
+              <div className="col-3 ">
+                <span className="badge text-bg-success">
+                  <i className="bi bi-currency-rupee"></i>
+                  35000
+                </span>
+              </div>
+              <div className="col-3 ">
+                <span className="badge text-bg-dark">
+                  <i className="bi bi-geo-alt me-1"></i>Noida
+                </span>
+              </div>
+            </div>
+            <p className="card-text lh-sm mb-2">
+              He/she should have a creative & thoughtful approach to requests &
+              collaborate...
+            </p>
+            <small className="d-block">2 days ago</small>
+            <a href="#" className="btn btn-primary mt-2">
+              Apply
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="col-md-5 col-lg-4 mb-3 mb-sm-1 m-4">
+        <div className="card shadow">
+          <div className="card-body">
+            <span className="badge text-bg-secondary mb-2">New</span>
+
+            <h5 className="card-title mb-0">Php Web devloper</h5>
+            <span>Umberala Infocare</span>
+            <div className="row mt-2 mb-1">
+              <div className="col-3 ">
+                <span className="badge text-bg-info">
+                  <i className="bi bi-briefcase me-1"></i>
+                  0-2 Yrs
+                </span>
+              </div>
+
+              <div className="col-3 ">
+                <span className="badge text-bg-success">
+                  <i className="bi bi-currency-rupee"></i>
+                  35000
+                </span>
+              </div>
+              <div className="col-3 ">
+                <span className="badge text-bg-dark">
+                  <i className="bi bi-geo-alt me-1"></i>Noida
+                </span>
+              </div>
+            </div>
+            <p className="card-text lh-sm mb-2">
+              He/she should have a creative & thoughtful approach to requests &
+              collaborate...
+            </p>
+            <small className="d-block">2 days ago</small>
+            <a href="#" className="btn btn-primary mt-2">
+              Apply
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default JobGrid;
